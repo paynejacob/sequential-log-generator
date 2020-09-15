8ed8a04abf0e13ecf2c3f058b56758426e351c95
Jacob Payne <jacob.payne@rancher.com>
2020-09-15 10:45:27 -0700

    fixed example

--- example.sh
+++ example.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if [ ! -f /usr/local/bin/sequential-log-generator ]; then
+if [ ! -e /usr/local/bin/sequential-log-generator ]; then
+  echo "Installing binary"
   wget -O /usr/local/bin/sequential-log-generator https://github.com/paynejacob/sequential-log-generator/raw/master/sequential-log-generator
   chmod +x /usr/local/bin/sequential-log-generator
 fi
 
-mkdir -n /examplelogs
+mkdir -p /examplelogs
 
 echo "starting log generator"
 sequential-log-generator /examplelogs/a.log,/examplelogs/b.log,/examplelogs/c.log 1000
